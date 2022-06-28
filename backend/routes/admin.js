c75c71f83a3e7b1fafcee88111a5b43162dccdcb
Laura <lhzig@protonmail.com>
2022-06-28 13:18:00 +0200

    Task 29 admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,20 +1,9 @@
 import express from "express";
-// import { albumsPost } from "../controllers/albumsController.js";
-// import authoriseUser from "../middleware/authoriseUser.js";
-// import albumValidator from "../validators/albumValidator.js";
-// import checkValidation from "../validators/checkValidation.js";
-// import requiredValues from "../validators/requiredValues.js";
-
+import { countUsers } from "../controllers/adminController"
 const router = express.Router();
-
 router.use(authoriseUser);
-
-router.get("/admin/:id/count", getAdminData)
-
-//router.post("/", requiredValues(["albumTitle", "band", "albumYear"]), albumValidator(), checkValidation, albumsPost);    // POST /albums
-
+router.get("/admin/:id/count", countUsers) // controller function 
 export default router;
 
-Query.prototype.count()
 
-const count = await User.countDocuments({})
+
